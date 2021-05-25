23d881eed1430291f5506906d516531a9a2f6048
Hassan Khan <hassanjkhan6@gmail.com>
2021-05-25 12:42:22 -0400

    removing aos parameters

--- assets/js/main.js
+++ assets/js/main.js
@@ -303,10 +303,11 @@ var functions = firebase.functions();
    */
   window.addEventListener("load", () => {
     AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
+      
+      //duration: 1000,
+      //easing: "ease-in-out",
       once: true,
-      mirror: false,
+      //mirror: false,
     });
   });
 })();
